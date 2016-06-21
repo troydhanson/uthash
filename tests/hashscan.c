b89904c08cabb556b030cdb340361474a39a12ad
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2016-06-21 21:05:38 +0000

    Remove duplicated `HASH_TO_BKT` logic from all the different hash functions.
    This speeds up `HASH_VALUE` by removing a dead assignment, and/or quiets
    a compiler warning from compilers that would warn about the dead assignment.
    But mostly it just simplifies the code.
    
    Unfortunately, this is an API-breaking change, for any client code that
    provides its own `HASH_FOO` function and passes `-DHASH_FUNCTION=HASH_FOO`,
    because I'm changing the signature of the `HASH_FUNCTION` function. Fortunately,
    at least it's a noisily breaking change; I don't think there's any way
    anyone's client code could quietly do the wrong thing after this patch.

--- tests/hashscan.c
+++ tests/hashscan.c
@@ -78,37 +78,20 @@ char *hash_fcns[] = {"???","JEN","BER","SFH","SAX","FNV","OAT","MUR"};
 /* given a peer key/len/hashv, reverse engineer its hash function */
 static int infer_hash_function(char *key, size_t keylen, uint32_t hashv)
 {
-    uint32_t obkt, ohashv, num_bkts=0x01000000; /* anything ok */
-    /* BER SAX FNV OAT JEN SFH */
-    HASH_JEN(key,keylen,num_bkts,ohashv,obkt);
-    if (ohashv == hashv) {
-        return JEN;
-    }
-    HASH_BER(key,keylen,num_bkts,ohashv,obkt);
-    if (ohashv == hashv) {
-        return BER;
-    }
-    HASH_SFH(key,keylen,num_bkts,ohashv,obkt);
-    if (ohashv == hashv) {
-        return SFH;
-    }
-    HASH_SAX(key,keylen,num_bkts,ohashv,obkt);
-    if (ohashv == hashv) {
-        return SAX;
-    }
-    HASH_FNV(key,keylen,num_bkts,ohashv,obkt);
-    if (ohashv == hashv) {
-        return FNV;
-    }
-    HASH_OAT(key,keylen,num_bkts,ohashv,obkt);
-    if (ohashv == hashv) {
-        return OAT;
-    }
-    HASH_MUR(key,keylen,num_bkts,ohashv,obkt);
-    if (ohashv == hashv) {
-        return MUR;
-    }
-    obkt++; // this quiets an unused variable warning. yes, this is a ugly hack
+    uint32_t ohashv;
+
+#define TRY_HASH(FOO)                \
+    HASH_##FOO(key, keylen, ohashv); \
+    if (ohashv == hashv) return FOO;
+
+    TRY_HASH(JEN)
+    TRY_HASH(BER)
+    TRY_HASH(SFH)
+    TRY_HASH(SAX)
+    TRY_HASH(FNV)
+    TRY_HASH(OAT)
+    TRY_HASH(MUR)
+
     return 0;
 }
 
