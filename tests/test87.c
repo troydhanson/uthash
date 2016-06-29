efd5ea2ed47997a8d2fbe108f18a4a4a619622ee
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2016-06-29 16:58:01 -0700

    Fix a typo and run "make astyle" on tests/test87.c.
    No functional change.

--- tests/test87.c
+++ tests/test87.c
@@ -1,75 +1,78 @@
 #include <stdio.h>
-#include <uthash.h>
+#include "uthash.h"
 
-typedef struct
-{
-  UT_hash_handle hh;
-
-  char name[32];
-  int weight;
+typedef struct {
+    UT_hash_handle hh;
+    char name[32];
+    int weight;
 } hstruct_t;
 
 #define ARRAY_LEN(x) (sizeof(x) / sizeof(*(x)))
-hstruct_t tst[] = {{{}, "muh1", 2}, {{}, "muh2", 8}, {{}, "muh3", 1}, {{}, "muh4", 8}, {{}, "muh5", 3}, {{}, "muh6", 5},
-  {{}, "muh7", 6}, {{}, "muh8", 15}, {{}, "muh9", 6}, {{}, "muh10", 9}, {{}, "muh11", 10}, {{}, "muh12", 43}};
+hstruct_t tst[] = {
+    {{0}, "muh1", 2},
+    {{0}, "muh2", 8},
+    {{0}, "muh3", 1},
+    {{0}, "muh4", 8},
+    {{0}, "muh5", 3},
+    {{0}, "muh6", 5},
+    {{0}, "muh7", 6},
+    {{0}, "muh8", 15},
+    {{0}, "muh9", 6},
+    {{0}, "muh10", 9},
+    {{0}, "muh11", 10},
+    {{0}, "muh12", 43}
+};
 hstruct_t *hTable;
 
-static int cmpfunc(hstruct_t *s1, hstruct_t *s2)
+static int cmpfunc(const hstruct_t *s1, const hstruct_t *s2)
 {
-  if(s1->weight < s2->weight)
-    return -1;
-  else if(s1->weight > s2->weight)
-    return 1;
-  else
-    return 0;
+    return (s1->weight < s2->weight) ? -1 : (s1->weight > s2->weight);
 }
 
 void printtable(void)
 {
-  hstruct_t *search;
+    hstruct_t *search;
 
-  for(search = hTable; search; search = search->hh.next)
-  {
-    printf("%d: %s\n", search->weight, search->name);
-  }
-  
-  printf("###\n");
+    for(search = hTable; search; search = search->hh.next) {
+        printf("%d: %s\n", search->weight, search->name);
+    }
+
+    printf("###\n");
 }
 
 void delitem(const char *name)
 {
-  hstruct_t *item;
-  
-  HASH_FIND_STR(hTable, name, item);
-  HASH_DEL(hTable, item);
-  printtable();
+    hstruct_t *item;
+
+    HASH_FIND_STR(hTable, name, item);
+    HASH_DEL(hTable, item);
+    printtable();
 }
 
 int main()
 {
-  int index;
+    int index;
 
-  for(index = 0; index < ARRAY_LEN(tst); index++)
-  {
-    HASH_SADD(hh, hTable, name[0], strlen(tst[index].name), &tst[index], cmpfunc);
-  } 
+    for(index = 0; index < ARRAY_LEN(tst); index++) {
+        HASH_SADD(hh, hTable, name[0], strlen(tst[index].name), &tst[index], cmpfunc);
+    }
 
-  printtable();
-  HASH_SORT(hTable, cmpfunc);
-  printtable();
+    printtable();
+    HASH_SORT(hTable, cmpfunc);
+    printtable();
 
-  delitem("muh12");
-  delitem("muh1");
-  delitem("muh7");
-  delitem("muh3");
-  delitem("muh9");
-  delitem("muh2");
-  delitem("muh11");
-  delitem("muh4");
-  delitem("muh6");
-  delitem("muh5");
-  delitem("muh8");
-  delitem("muh10");
+    delitem("muh12");
+    delitem("muh1");
+    delitem("muh7");
+    delitem("muh3");
+    delitem("muh9");
+    delitem("muh2");
+    delitem("muh11");
+    delitem("muh4");
+    delitem("muh6");
+    delitem("muh5");
+    delitem("muh8");
+    delitem("muh10");
 
-  return 0;
+    return 0;
 }
