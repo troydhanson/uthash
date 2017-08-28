0abcbcd1f01bbe0c4703e91c7886c0cc24d3393d
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-08-28 11:51:50 -0700

    Update test92 to work also without HASH_BLOOM.

--- tests/test92.c
+++ tests/test92.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-#define HASH_BLOOM 16
 #define HASH_OOM_OK 1
+
+#include "uthash.h"
 
 #undef uthash_malloc
 #undef uthash_free
-#undef uthash_fatal
+#undef uthash_oom
 #define uthash_malloc(sz) alt_malloc(sz)
 #define uthash_free(ptr,sz) alt_free(ptr)
-#define uthash_oom(e) do{((example_user_t*)e)->mem_failed=1;}while(0)
+#define uthash_oom(e) do{(e)->mem_failed=1;}while(0)
 #define all_select(a) 1
-
-#include "uthash.h"
 
 typedef struct example_user_t {
     int id;
@@ -22,17 +21,15 @@ typedef struct example_user_t {
 } example_user_t;
 
 static int malloc_cnt = 0;
-static int malloc_failed;
+static int malloc_failed = 0;
 static int free_cnt = 0;
 
 static void *alt_malloc(size_t sz)
 {
     if (--malloc_cnt <= 0) {
-        // printf("fail malloc req for %ld\n", sz);
         malloc_failed = 1;
         return 0;
     }
-    // printf("OK malloc req for %ld\n", sz);
     malloc_failed = 0;
     return malloc(sz);
 }
@@ -42,104 +39,122 @@ static void alt_free(void *ptr) {
     free(ptr);
 }
 
+static void complain(int index, example_user_t *users, example_user_t *user)
+{
+    int expected_frees = (3 - index);
+    if (users) {
+        printf("%d: users hash must be empty\n", index);
+    }
+    if (user->hh.tbl) {
+        printf("%d hash table must be empty\n", index);
+    }
+    if (free_cnt != expected_frees) {
+        printf("%d Expected %d frees, only had %d\n", index, expected_frees, free_cnt);
+    }
+    if (user->mem_failed != 1) {
+        printf("%d Expected user->mem_failed(%d) to be 1\n", index, user->mem_failed);
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    example_user_t *users = NULL;
+    example_user_t *user = (example_user_t*)malloc(sizeof(example_user_t));
+    example_user_t *test;
+    example_user_t *users2 = NULL;
+    int id = 0;
+    int i;
+    int saved_cnt;
 
-#define complain(a,c) do { \
-    if (users) { \
-        printf("%d: users hash must be empty\n", a); \
-    } \
-    if (user->hh.tbl) { \
-        printf("%d hash table must be empty\n", a); \
-    } \
-    if (free_cnt != c) { \
-        printf("%d Expected %d frees, only had %d\n", a, c, free_cnt); \
-    } \
-    if (user->mem_failed != 1) { \
-        printf("%d Expected user->mem_failed(%d) to be 1\n", a, user->mem_failed); \
-    } \
-} while (0)
-
-    example_user_t * users = 0, * user, * test, * users2 = 0;
-    int id = 0, i, saved_cnt;
-
-    malloc_cnt = 3; // bloom filter must fail
-    user = (example_user_t*)malloc(sizeof(example_user_t));
     user->id = id;
+
+#ifdef HASH_BLOOM
+    malloc_cnt = 3; // bloom filter must fail
     user->mem_failed = 0;
     user->hh.tbl = (UT_hash_table*)1;
     free_cnt = 0;
     HASH_ADD_INT(users, id, user);
-    complain(1, 2);
+    complain(1, users, user);
+#endif /* HASH_BLOOM */
 
     malloc_cnt = 2; // bucket creation must fail
     user->mem_failed = 0;
     free_cnt = 0;
     user->hh.tbl = (UT_hash_table*)1;
     HASH_ADD_INT(users, id, user);
-    complain(2, 1);
+    complain(2, users, user);
 
     malloc_cnt = 1; // table creation must fail
     user->mem_failed = 0;
     free_cnt = 0;
     user->hh.tbl = (UT_hash_table*)1;
     HASH_ADD_INT(users, id, user);
-    complain(3, 0);
+    complain(3, users, user);
 
     malloc_cnt = 4; // hash must create OK
     user->mem_failed = 0;
     HASH_ADD_INT(users, id, user);
     if (user->mem_failed) {
-        printf("mem failed flags must be 0\n");
+        printf("mem_failed must be 0, not %d\n", user->mem_failed);
     }
     HASH_FIND_INT(users,&id,test);
     if (!test) {
         printf("test user ID %d not found\n", id);
     }
 
+    if (HASH_COUNT(users) != 1) {
+        printf("Got HASH_COUNT(users)=%d, should be 1\n", HASH_COUNT(users));
+    }
+
     // let's add users until expansion fails.
+    malloc_failed = 0;
     free_cnt = 0;
-    while (1) {
+    malloc_cnt = 1;
+    for (id = 1; 1; ++id) {
         user = (example_user_t*)malloc(sizeof(example_user_t));
-        if ((user->id = ++id) == 1000) {
+        user->id = id;
+        if (id >= 1000) {
             // prevent infinite, or too long of a loop here
-            printf("too many allocs before memory request");
+            puts("too many allocs before memory request");
             break;
         }
         user->hh.tbl = (UT_hash_table*)1;
         HASH_ADD_INT(users, id, user);
         if (malloc_failed) {
+            if (id < 10) {
+                puts("there is no way your bucket size is <= 10");
+            }
+
+            if (user->hh.tbl) {
+                puts("user->hh.tbl should be NULL after failure");
+            } else if (user->mem_failed != 1) {
+                printf("mem_failed should be 1 after failure, not %d\n", user->mem_failed);
+            }
 
             if (free_cnt != 0) {
-                printf("something unexpected was freed\n");
-            }
-
-            if (id < 10) {
-                printf("there is no way your bucket size is <= 10\n");
-            }
-
-            if (user->hh.tbl || !user->mem_failed) {
-                printf("mem_failed must be 1 after extension failed\n");
+                printf("Expected 0 frees, had %d\n", free_cnt);
             }
 
             // let's make sure all previous IDs are there.
-            for (int i=0; i<id; i++) {
+            for (int i=0; i<id; ++i) {
                 HASH_FIND_INT(users,&i,test);
-                if (!test) {
+                if (test == NULL) {
                     printf("test user ID %d not found\n", i);
                 }
             }
 
             // let's try to add again, but with mem_failed set to 0
-            user->hh.tbl = 0;
+            user->hh.tbl = NULL;
             user->mem_failed = 0;
             malloc_failed = 0;
             HASH_ADD_INT(users, id, user);
             if (!malloc_failed) {
-                printf("malloc not attempted\n");
+                puts("malloc should have been attempted");
             }
-            if (!user->mem_failed || user->hh.tbl) {
-                printf("mem_failed must be 1 after extension failed second time\n");
+            if (user->hh.tbl) {
+                puts("user->hh.tbl should be NULL after second failure");
+            } else if (user->mem_failed != 1) {
+                printf("mem_failed should be 1 after second failure, not %d\n", user->mem_failed);
             }
 
             break;
@@ -150,29 +165,37 @@ int main(int argc, char *argv[])
     // let's double the size of the table we've already built.
     saved_cnt = id;
 
-    for (i=0; i<saved_cnt; i++) {
+    if (HASH_COUNT(users) != saved_cnt) {
+        printf("Got HASH_COUNT(users)=%d, should be %d\n", HASH_COUNT(users), saved_cnt);
+    }
+
+    for (i=0; i < saved_cnt; i++) {
         user = (example_user_t*)malloc(sizeof(example_user_t));
         user->id = ++id;
-        malloc_cnt = 4;
+        malloc_cnt = 20;  // don't fail
         HASH_ADD_INT(users, id, user);
     }
 
     HASH_ITER(hh, users, user, test) {
-        users->mem_failed = 0;
+        user->mem_failed = 0;
     }
+
+// HASH_SELECT calls uthash_oom() with an argument of type (void*).
+#undef uthash_oom
+#define uthash_oom(e) do{((example_user_t*)e)->mem_failed=1;}while(0)
 
     malloc_cnt = 0;
     free_cnt = 0;
     HASH_SELECT(hh2, users2, hh, users, all_select);
     if (users2) {
-        printf("Nothing could have been copied into users2\n");
+        puts("Nothing should have been copied into users2");
     }
     HASH_ITER(hh, users, user, test) {
         if (user->hh2.tbl) {
             printf("User ID %d has tbl at %p\n", user->id, (void*)user->hh2.tbl);
         }
-        if (!user->mem_failed) {
-            printf("User ID %d has clear mem_failed, should be 1\n", user->id);
+        if (user->mem_failed != 1) {
+            printf("User ID %d has mem_failed(%d), should be 1\n", user->id, user->mem_failed);
         }
         user->mem_failed = 0;
     }
@@ -189,25 +212,24 @@ int main(int argc, char *argv[])
     // failures, the bucket threshold may get triggered on arbitrary
     // elements, depending on the hash function.
 
-    // printf("users2 size is %d, users is %d\n", HASH_CNT(hh2, users2), HASH_COUNT(users));
     saved_cnt = 0;
     HASH_ITER(hh, users, user, test) {
         example_user_t * user2;
         HASH_FIND(hh2, users2, &user->id, sizeof(int), user2);
         if (user2) {
             if (!user->hh2.tbl) {
-                printf("User ID %d has tbl at %p, expected !0\n", user->id, (void*)user->hh2.tbl);
+                printf("User ID %d has tbl==NULL\n", user->id);
             }
-            if (user->mem_failed) {
-                printf("User ID %d has !0 mem_failed, expected is 0\n", user->id);
+            if (user->mem_failed != 0) {
+                printf("User ID %d has mem_failed(%d), expected 0\n", user->id, user->mem_failed);
             }
         } else {
             saved_cnt++;
             if (user->hh2.tbl) {
                 printf("User ID %d has tbl at %p, expected 0\n", user->id, (void*)user->hh2.tbl);
             }
-            if (!user->mem_failed) {
-                printf("User ID %d has 0 mem_failed, expected is 1\n", user->id);
+            if (user->mem_failed != 1) {
+                printf("User ID %d has mem_failed(%d), expected is 1\n", user->id, user->mem_failed);
             }
         }
     }
@@ -217,8 +239,7 @@ int main(int argc, char *argv[])
                 saved_cnt, HASH_CNT(hh2, users2), HASH_COUNT(users));
     }
 
-    printf("End\n");
+    puts("End");
 
     return 0;
-
 }
