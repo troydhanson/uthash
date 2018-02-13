a116de7ecadf2ca94c150fa239c116f6e6cd7a17
silvioprog <silvioprog@gmail.com>
2018-02-13 17:34:45 -0300

    utstring.h: improves the UT_string member commenting.

--- src/utstring.h
+++ src/utstring.h
@@ -44,8 +44,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endif
 
 typedef struct {
-    char *d;
-    size_t n; /* allocd size */
+    char *d; /* string buffer */
+    size_t n; /* allocated size */
     size_t i; /* index of first unused byte */
 } UT_string;
 
