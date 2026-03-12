4f77edd2323e22501e08b81edac961e20e9ae5b3
xunicatt <aniketbiswas625@gmail.com>
2026-03-12 11:07:51 +0530

    tests: updated test101

--- tests/test101.c
+++ tests/test101.c
@@ -14,18 +14,18 @@ int main(void) {
     for(i = 1; i <= 1024; i <<= 1) {
         node = malloc(sizeof(*node));
         node->value = i;
-        LL_APPEND(list, node);
+        LL_PREPEND(list, node);
     }
 
     LL_REVERSE(list);
 
     node = malloc(sizeof(*node));
     node->value = 0;
-    LL_APPEND(list, node);
+    LL_PREPEND(list, node);
 
     node = malloc(sizeof(*node));
     node->value = 2048;
-    LL_PREPEND(list, node);
+    LL_APPEND(list, node);
 
     LL_FOREACH(list, node) {
         printf("%d\n", node->value);
